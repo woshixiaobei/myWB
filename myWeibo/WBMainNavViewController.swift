bc31015ac0aaec97d9f7466110f288714afbccf4
wangmingzhu <wmz@qq.com>
2016-07-01 20:34:25 +0800

    添加中间的撰写按钮,并设置监听事件,&修改了tabbar的字体大小

--- myWeibo/WBMainNavViewController.swift
+++ myWeibo/WBMainNavViewController.swift
@@ -12,24 +12,14 @@ class WBMainNavViewController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //navigationBar.isHidden = true
+        
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+extension WBMainNavViewController {
+
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- myWeibo/myWeibo/Classes/View/Main/WBMainTabBarController.swift
+++ myWeibo/myWeibo/Classes/View/Main/WBMainTabBarController.swift
@@ -14,17 +14,45 @@ class WBMainTabBarController: UITabBarController {
         super.viewDidLoad()
 
         setupChildControllers()
+        setupComposeButton()
     }
 
+    //撰写按钮的监听方法
+    func composeStatus() {
+    
+        print("撰写微博")
+    
+    }
+    //MARK:添加撰写按钮
+    private lazy var composeButton: UIButton = UIButton.cz_imageButton("tabbar_compose_icon_add", backgroundImageName: "tabbar_compose_button")
+    
 }
 
+
+//MARK:添加子控制器
 extension WBMainTabBarController {
 
+   //撰写按钮
+    private func setupComposeButton() {
+    
+        tabBar.addSubview(composeButton)
+        let count = CGFloat(childViewControllers.count)
+        let w = tabBar.bounds.width / count - 1
+
+        composeButton.frame = tabBar.bounds.insetBy(dx: 2 * w, dy: 0)
+        print("撰写按钮的宽度\(composeButton.bounds.width)")
+//         按钮监听方法
+        composeButton.addTarget(self, action: #selector(composeStatus), for: .touchUpInside)
+    }
+
+
+    
     //添加多个子控制器
    private func setupChildControllers() {
         
         let array = [["clsName":"WBHomeViewController","title":"首页","imageName":"home"],
                      ["clsName":"WBMessageViewController","title":"消息","imageName":"message_center"],
+                     ["clsName":"UIViewController"],
                      ["clsName":"WBDiscoverViewController","title":"发现","imageName":"discover"],
                  ["clsName":"WBProflieViewController","title":"我","imageName":"profile"],
                  
@@ -58,7 +86,8 @@ extension WBMainTabBarController {
         
         //设置tabbar 的字体颜色
         vc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.orange()], for: .highlighted)
-      
+        vc.tabBarItem.setTitleTextAttributes([NSFontAttributeName:UIFont.systemFont(ofSize: 12)], for: UIControlState(rawValue: 0))
+        
         let nav = WBMainNavViewController(rootViewController: vc)
         return nav
         

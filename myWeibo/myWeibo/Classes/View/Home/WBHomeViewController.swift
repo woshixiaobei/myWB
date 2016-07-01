8c55ba0c7ba1aab687c02493dddf20acdcd6941f
wangmingzhu <wmz@qq.com>
2016-07-01 17:19:53 +0800

    修改启动图标,设置应用程序主入口

---
+++ myWeibo/myWeibo/Classes/View/Home/WBHomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WBHomeViewController.swift
+//  myWeibo
+//
+//  Created by 小贝 on 16/7/1.
+//  Copyright © 2016年 小贝. All rights reserved.
+//
+
+import UIKit
+
+class WBHomeViewController: WBBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
